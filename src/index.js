64138172e8c88fd2d10e8b68c7a8f51987f6946d
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-07-06 16:14:19 +0530

    Implement application logic for tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('using webpack');
+import {todoList, Todo} from './todo';
+
+let todo1 = Todo('todo1', 'just checkin', 'today', 'Home');
+todoList.add(todo1);
+console.log(todo1);
+console.log(todoList.getList());
+console.log(todo1.getTodo());
+
+todo1.edit('todo2', 'checkin edit', 'tomorrow', 'default');
+console.log(todo1.getTodo());
+
+todo1.moveToProject('home');
+todo1.reschedule('today');
+
+let subTaskList = [];
+subTaskList.push(Todo('subtask1', 'do subtask', 'today', 'misc'));
+todo1.addChecklist(subTaskList);
+console.log(todo1.getTodo());
+
+console.log('output for mark');
+todo1.markAsCompleted();
+todoList.remove(todo1);
---
+++ src/todo.js
@@ -0,0 +1,68 @@
+const todoList = (function TodoList() {
+    const todos = [];
+    function add(todo) {
+        todos.push(todo);
+    }
+
+    function remove(todo) {
+        let index = todos.findIndex(todo_ => todo_.title === todo.title);
+        todos.splice(index, 1);
+    }
+
+    function getList() {
+        return todos;
+    }
+
+    return {add, remove, getList};
+})();
+
+function Todo (title, notes, dueDate, project="Home") {
+    let title_ = title;
+    let notes_ = notes;
+    let dueDate_ = dueDate;
+    let project_ = project;
+
+    let checkList_ = [];
+    let completed_ = false;
+
+    function edit(title, notes, dueDate, project) {
+        title_ = title;
+        notes_ = notes;
+        dueDate_ = dueDate;
+        project_ = project;
+    }
+
+    function moveToProject(project) {
+        project_ = project;
+    }
+
+    function reschedule(newDate) {
+        dueDate_ = newDate;
+    }
+
+    function addChecklist(list) {
+        checkList_ = list;
+    }
+
+    function markAsCompleted() {
+        // check if all subtasks are completed
+        let subtasks = checkList_.map(subtask => subtask.getTodo());
+        let completed = subtasks.every(subtask => subtask.completed_);
+        if(completed) {
+
+        } else {
+            console.log("please complete all subtasks before marking the task as complete");
+        }
+    }
+
+    function getTodo() {
+        return {title_, notes_, dueDate_, project_, checkList_, completed_};
+    }
+
+    return {edit, moveToProject, reschedule, addChecklist, markAsCompleted,getTodo};
+}
+
+export {todoList, Todo};
+
+
+
